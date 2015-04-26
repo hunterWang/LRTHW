6b321b0352950a02b3e50863779d9dc197f51327
Hunter Wang <wch516@gmail.com>
2015-04-26 11:42:08 +0000

    add  demo project

---
+++ projects/demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dwmo::Application.config.secret_key_base = 'a72953beb4f55178f464b2aa32c83fea5a3b9b1b97a4738861c4f46b04a75f8628e8de284318d9b820291538dee664fdef785d84be6c37d22506cf5f4e6b5606'
