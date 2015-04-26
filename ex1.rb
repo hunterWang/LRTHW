2d6a657cd612199120e0e1cf49d1e4d823811645
Hunter Wang <wch516@gmail.com>
2015-04-26 12:01:35 +0000

    arrange folder

--- ex1.rb
+++
@@ -1,7 +0,0 @@
-puts "hELLO wORLD"
-puts "Hello Again"
-puts "i'll like type this"
-puts "this is fun"
-puts "Yay~ printing"
-puts "i'd must rather you 'not' "
-puts 'i "said" do not touch this '
--- ex2.rb
+++
@@ -1,7 +0,0 @@
-# a comment mthis is so you can read your program later 
-# anything after "#" is ignored by ruby
-puts "i could cod like this " # and the comment after is ignored
-
-# puts "this won't work"
-
-puts "this will work"
