189e7ea7cca29e49da848571f02446a46d8fac58
Hunter Wang <wch516@gmail.com>
2015-05-01 05:02:25 +0000

    try db

--- projects/demo/db/schema.rb
+++ projects/demo/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426114949) do
+ActiveRecord::Schema.define(version: 20150501045039) do
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.text     "descriptnio"
+    t.boolean  "is_public"
+    t.integer  "capacity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", force: true do |t|
     t.string   "name"
