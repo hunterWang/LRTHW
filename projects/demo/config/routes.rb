d9ef670a5d4093e4ea2dc017c592fa7bb83d38e3
Hunter Wang <wch516@gmail.com>
2015-05-02 07:15:37 +0000

    validation

--- projects/demo/config/routes.rb
+++ projects/demo/config/routes.rb
@@ -4,6 +4,8 @@ Dwmo::Application.routes.draw do
     get "welcome/say_hello" => "welcome#say"
     get "welcome" => "welcome#index"
     
+    match ':controller(/:action(/id(.:format)))', :via => :all? 
+    
     root :to =>"welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
