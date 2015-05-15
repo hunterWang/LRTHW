6de256173ae0832c9eea29ce8b680740ae1b88fe
Hunter Wang <wch516@gmail.com>
2015-05-15 09:27:34 +0000

    change route to topics/index

---
+++ projects/railsbridge/suggestotron/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Suggestotron::Application.config.secret_key_base = '14b80ed93537d377b9a18a6b44ed988a82975e8e8d0c1b292fcfa1c66022350c4cb1ae4fdce18434682f4a67a44741b782eabefd95f5ae3d4d95a947e2079936'
