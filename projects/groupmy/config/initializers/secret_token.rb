d36cbf4d322baaf5b0c2197a26bd27b6bd0a6fcd
hunterWang <wch516@hotmail.com>
2015-05-21 21:54:40 +0800

    hello world

---
+++ projects/groupmy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupmy::Application.config.secret_key_base = '03ccb23d9354c9dddeb52ea496ad31f9a0642aa74a58f8127a14db62801c2fdd05c6d5ff4f14e3c50333eca4ce7c0603674a5ef35981dac30cb1b25d56a981d7'
