33ddfdc3829b524d0ebd53d05d49aacc751e136e
hunterWang <wch516@hotmail.com>
2015-05-25 22:21:30 +0800

    remove first app and add group101

---
+++ projects/group101/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Group101::Application.config.secret_key_base = '74fc861d73976c183019cd370b0a445f2ebd5dedd3e811da4d0fe93daa1f45290c07e02f2dd126ebd05bf06631eb661dd994eeaacde914f2a4c7a56dc22e740d'
---
+++ projects/group101/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Group101::Application.config.session_store :cookie_store, key: '_group101_session'
